4b0826a4bd222530c270583f78da7c9e9088a1fc
NSmith <nick_smith@ukr.net>
2020-10-13 09:56:35 +0300

    simple activity result

---
+++ p0291simpleactivityresult/src/main/java/ua/startandroid/p0291simpleactivityresult/NameActivity.java
@@ -0,0 +1,33 @@
+package ua.startandroid.p0291simpleactivityresult;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etName;
+    Button btnOK;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+
+        etName = (EditText) findViewById(R.id.etName);
+        btnOK = (Button) findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("name", etName.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
