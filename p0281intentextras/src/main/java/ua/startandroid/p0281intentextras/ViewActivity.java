9b3c21a0ca459913a7de64844ff935da0aa93f86
NSmith <nick_smith@ukr.net>
2020-10-13 08:46:43 +0300

    intent extras

---
+++ p0281intentextras/src/main/java/ua/startandroid/p0281intentextras/ViewActivity.java
@@ -0,0 +1,27 @@
+package ua.startandroid.p0281intentextras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ViewActivity extends AppCompatActivity {
+
+    TextView tvView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        tvView = (TextView) findViewById(R.id.tvView);
+
+        Intent intent = getIntent();
+
+        String fName = intent.getStringExtra("fname");
+        String lName = intent.getStringExtra("lname");
+
+        tvView.setText("Entered name is: " + fName + " " + lName);
+    }
+}
